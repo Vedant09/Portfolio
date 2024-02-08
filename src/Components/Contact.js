822619e9cc3f978af4e410cf2cee1cd23cdce7bf
Vedant09 <vedantvy98@gmail.com>
2024-02-08 17:02:16 -0500

    Contact me section2

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -2,12 +2,41 @@ import React from 'react'
 import SectionTitle from './SectionTitle'
 
 function ContactForm() {
+    const [formdata, setFormData] = React.useState({
+        name: "",
+        email: "",
+        message: ""
+    });
+
+    function handleSubmit(event) {
+        const { name, email, message } = event.target;
+
+        setFormData(prevData => ({
+            ...prevData,
+            [name]: name.value,
+            [email]: email.value,
+            [message]: message.value
+        }));
+    }
+
+
+    function handleInputChange(event) {
+        const { name, value } = event.target;
+
+        setFormData(prevData => ({
+            ...prevData,
+            [name]: value
+        }));
+    }
+    console.log("hi",formdata)
     return (
-        <form className='contact-form'>
+        <form className='contact-form' onSubmit={handleSubmit}>
             <input
-            className='text-input'
+                className='text-input'
                 type="text"
                 name="name"
+                value={formdata.name}
+                onChange={handleInputChange}
                 placeholder='Name'
             />
             <br />
@@ -15,6 +44,8 @@ function ContactForm() {
                 className='text-input'
                 type="text"
                 name="email"
+                value={formdata.email}
+                onChange={handleInputChange}
                 placeholder='E-mail'
             />
             <br />
@@ -22,7 +53,9 @@ function ContactForm() {
                 className='text-input-area'
                 type="textArea"
                 name="message"
-                placeholder='Messasge'
+                value={formdata.message}
+                onChange={handleInputChange}
+                placeholder='Message'
             />
             <br />
             <button className='form-submit'>Submit</button>
@@ -39,7 +72,7 @@ export default function Contact() {
             />
             <ContactForm />
             <div className='contact-phone-email-div'>
-                <p>Phone Number : +1(571)-591-8877</p><br/>
+                <p>Phone Number : +1(571)-591-8877</p><br />
                 <p>E-mail : vyelsang@gmu.edu</p>
             </div>
         </div>
