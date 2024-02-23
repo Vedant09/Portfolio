c7fa1ae5c56166c148b78fb7f2a6a309e48df635
Vedant09 <vedantvy98@gmail.com>
2024-02-22 23:23:19 -0500

    Lazzy loading

--- src/Components/Elements.js
+++ src/Components/Elements.js
@@ -1,16 +1,21 @@
-import React from 'react'
+import React from "react";
 
 export default function Elements(props) {
-    function goto() {
-        window.open(props.items.link, '_blank');
-      }
-    return ( 
-          <div className='card-div' onClick={goto}>
-              <div className='cards-each'>
-                <img className='project-image' src={props.items.img} alt="img"/>
-                <h3 className='project-name'>{props.items.type}</h3>
-                <p>{props.items.city}</p>
-            </div>
-        </div>
-    )
-} 
+  function goto() {
+    window.open(props.items.link, "_blank");
+  }
+  return (
+    <div className="card-div" onClick={goto}>
+      <div className="cards-each">
+        <img
+          className="project-image"
+          src={props.items.img}
+          alt="img"
+          loading="lazy"
+        />
+        <h3 className="project-name">{props.items.type}</h3>
+        <p>{props.items.city}</p>
+      </div>
+    </div>
+  );
+}
