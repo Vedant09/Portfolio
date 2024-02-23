ffa80691b5f46efe1180c958096eacbd9a60a441
Vedant09 <vedantvy98@gmail.com>
2024-02-22 22:31:10 -0500

    contact rrmoved

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import './App.css';
-import Profile from './Components/Profile';
-import Navbar from './Components/Navbar';
-import WorkExp from './Components/Work Experience';
-import Projects from './Components/Projects';
-import Skills from './Components/Skills';
-import Footer from './Components/Footer'
-import Contact from './Components/Contact';
+import "./App.css";
+import Profile from "./Components/Profile";
+import Navbar from "./Components/Navbar";
+import WorkExp from "./Components/Work Experience";
+import Projects from "./Components/Projects";
+import Skills from "./Components/Skills";
+import Footer from "./Components/Footer";
+// import Contact from './Components/Contact';
 
 function App() {
   return (
-    <div className='App'>
-      <Navbar/>
-      <Profile/>
-      <Projects/>
-      <Skills/>
-      <WorkExp/>
-      <Contact/>
-      <Footer/>
+    <div className="App">
+      <Navbar />
+      <Profile />
+      <Projects />
+      <Skills />
+      <WorkExp />
+      {/* <Contact/> */}
+      <Footer />
     </div>
   );
 }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,14 +1,22 @@
-import React from 'react'
-import {Link} from 'react-scroll'
+import React from "react";
+import { Link } from "react-scroll";
 /*Link will take the unique of that component ID*/
-export default function Navbar(){
-    return(
+export default function Navbar() {
+  return (
     <div className="navbar">
-        <Link to="home" smooth={true} duration={500}>Introduction</Link>
-        <Link to="projects" smooth={true} duration={500}>Projects</Link>
-        <Link to="skills" smooth={true} duration={500}>Skills</Link>
-        <Link to="workexp" smooth={true} duration={500}>My Experience</Link>
-        <Link to="contact" smooth={true} duration={500}>Contact Me</Link>
+      <Link to="home" smooth={true} duration={500}>
+        Introduction
+      </Link>
+      <Link to="projects" smooth={true} duration={500}>
+        Projects
+      </Link>
+      <Link to="skills" smooth={true} duration={500}>
+        Skills
+      </Link>
+      <Link to="workexp" smooth={true} duration={500}>
+        My Experience
+      </Link>
+      {/* <Link to="contact" smooth={true} duration={500}>Contact Me</Link> */}
     </div>
-    )
+  );
 }
