26684e97a62d6848d59c1652f03da3c1217f61cb
Vedant09 <vedantvy98@gmail.com>
2024-02-22 23:16:01 -0500

    image

--- src/DataFiles/ProjectsData.js
+++ src/DataFiles/ProjectsData.js
@@ -1,58 +1,60 @@
-import C2C from '../Images/c2c.png'
-import Vectorent from '../Images/vectorent.png'
-import Bias from '../Images/bias-mitigation.png'
-import SCM from '../Images/scm.png'
-import More from '../Images/other-projects.png'
-import Vedant from '../Images/Vedant.png'
-import Mhhr from '../Images/mhhr.png'
+import C2C from "../Images/c2c.jpg";
+import Vectorent from "../Images/vectorent.jpg";
+import Bias from "../Images/bias-mitigation.jpg";
+import SCM from "../Images/scm.jpg";
+import More from "../Images/other-projects.jpg";
+import Vedant from "../Images/Vedant.jpg";
+import Mhhr from "../Images/mhhr.jpg";
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default [    
-    {
-        id : 1,
-        img : Bias,
-        type : "Bias Mitigation",
-        link : "https://github.com/Vedant09/Mitigation-in-COMPAS-Algorithm",
-        city : "Python",
-    },
-    {
-        id : 2,
-        img : C2C,
-        type : "C2C",
-        link : "http://107.23.248.22/",
-        city : "Python, ReactJs, Ansible",
-    },
-    {
-        id : 3,
-        img : Vectorent,
-        type : "Vectorent",
-        link : "https://github.com/Vedant09/VectoRent",
-        city : "Reactjs, Nodejs, OracleSQL",
-    },
-    {
-        id : 4,
-        img : SCM,
-        type : "SCM Blockchain",
-        link : "https://github.com/Vedant09/Pharmaceuticals-SCM-BlockChain",
-        city : "Web3Js, ReactJs, Flutter",
-    },{
-        id : 5,
-        img : Vedant,
-        type : "Personal Portfolio",
-        link : "https://github.com/Vedant09/Portfolio",
-        city : "ReactJs",
-    },
-    {
-        id : 6,
-        img : Mhhr,
-        type : "MHHR Properties",
-        link : "https://mhhrproperties.com/",
-        city : "ReactJs, NodeJs, PostgreSQL",
-    },{
-        id : 7,
-        img : More,
-        type : "Other Projects",
-        link : "https://github.com/Vedant09?tab=repositories",
-        city : "2019-Present",
-    }
-]
+export default [
+  {
+    id: 1,
+    img: Bias,
+    type: "Bias Mitigation",
+    link: "https://github.com/Vedant09/Mitigation-in-COMPAS-Algorithm",
+    city: "Python",
+  },
+  {
+    id: 2,
+    img: C2C,
+    type: "C2C",
+    link: "http://107.23.248.22/",
+    city: "Python, ReactJs, Ansible",
+  },
+  {
+    id: 3,
+    img: Vectorent,
+    type: "Vectorent",
+    link: "https://github.com/Vedant09/VectoRent",
+    city: "Reactjs, Nodejs, OracleSQL",
+  },
+  {
+    id: 4,
+    img: SCM,
+    type: "SCM Blockchain",
+    link: "https://github.com/Vedant09/Pharmaceuticals-SCM-BlockChain",
+    city: "Web3Js, ReactJs, Flutter",
+  },
+  {
+    id: 5,
+    img: Vedant,
+    type: "Personal Portfolio",
+    link: "https://github.com/Vedant09/Portfolio",
+    city: "ReactJs",
+  },
+  {
+    id: 6,
+    img: Mhhr,
+    type: "MHHR Properties",
+    link: "https://mhhrproperties.com/",
+    city: "ReactJs, NodeJs, PostgreSQL",
+  },
+  {
+    id: 7,
+    img: More,
+    type: "Other Projects",
+    link: "https://github.com/Vedant09?tab=repositories",
+    city: "2019-Present",
+  },
+];
